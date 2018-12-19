c9b1e82f1cb822f2f57286bb459e313418daa54d
hamster197 <hamster197@mail.ru>
2018-12-19 11:51:46 +0300

    Houses for yandex

--- crm/views.py
+++ crm/views.py
@@ -975,9 +975,9 @@ def YandexFeedview(request):
 
 ## New Yandex for all(with out feiks)
 def NewYandexFeedview(request):
-    #post = flat_obj.objects.filter(domclick='Да', domclick_pub='Да', type='flat').order_by('-pk')[:20]
-    post = flat_obj.objects.filter(ya_verifed_pr='Да', type='flat').order_by('-pk')
-    doma = flat_obj.objects.filter(domclick='Да1', type='house').order_by('-datep')
+    post = flat_obj.objects.filter(domclick='Да1', domclick_pub='Да', type='flat').order_by('-pk')[:20]
+    #post = flat_obj.objects.filter(ya_verifed_pr='Да', type='flat').order_by('-pk')
+    doma = flat_obj.objects.filter(domclick='Да', type='house').order_by('-datep')
     uchastoc = flat_obj.objects.filter(domclick='Да1', type='uchastok').order_by('-datep')
     gal = flat_obj_gal.objects.all()
     #post = flat_obj.objects.filter(author.userprofile1.tel='' ).order_by('-datep')
