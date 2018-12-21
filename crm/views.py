6bab11c75515d0a2abfcb2dcbecf1b45e534d5d6
hamster197 <hamster197@mail.ru>
2018-12-21 10:48:56 +0300

    yandex feed fix

--- crm/views.py
+++ crm/views.py
@@ -977,7 +977,7 @@ def YandexFeedview(request):
 def NewYandexFeedview(request):
     post = flat_obj.objects.filter(domclick='Да1', domclick_pub='Да', type='flat').order_by('-pk')[:20]
     #post = flat_obj.objects.filter(ya_verifed_pr='Да', type='flat').order_by('-pk')
-    doma = flat_obj.objects.filter(domclick='Да', type='house').order_by('-datep')
+    doma = flat_obj.objects.filter(domclick='Да', type='house').order_by('-datep')[:20]
     uchastoc = flat_obj.objects.filter(domclick='Да1', type='uchastok').order_by('-datep')
     gal = flat_obj_gal.objects.all()
     #post = flat_obj.objects.filter(author.userprofile1.tel='' ).order_by('-datep')
